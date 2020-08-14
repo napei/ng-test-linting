55b50db61344e60016fe8da3a602cab7c3d05be9
Nathaniel Peiffer <nathaniel@peiffer.com.au>
2020-08-14 23:01:30 +1000

    break code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'test',
-    children: [
-      {
-        path: '',
-        loadChildren: () =>
-          import('./pages/test/test.module').then((m) => m.TestModule),
-      },
-    ],
-  },
+  // {
+  //   path: 'test',
+  //   children: [
+  //     {
+  //       path: '',
+  //       loadChildren: () =>
+  //         import('./pages/test/test.module').then((m) => m.TestModule),
+  //     },
+  //   ],
+  // },
 ];
 
 @NgModule({
